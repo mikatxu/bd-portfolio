518de65c37419d2a9f2c43a622d81d064674c562
Mikatxu <memorial666@free.fr>
2016-03-16 14:40:15 +0100

    beginning to write the documentaiton

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # bd-portfolio
 
 A JQuery custom slider in a WordPress plugin.
+
+## Usage
+
+Le portfolio est structuré par des shortcodes.
+
+### Syntaxe de base
+
+[bdp-open]
+[bdp-more-info]
+[/bdp-more-info]
+[bdp-content]
+...
+[bdp-content-end]
+[bdp-samecategory]
+[bdp-close]
+
+1. [bdp-open]
+
+Ouvre nécessairement le portfolio.
+
+2. [bdp-more-info][/bdp-more-info]
--- bd-portfolio-shortcodes.php
+++ bd-portfolio-shortcodes.php
@@ -8,7 +8,7 @@ function bd_portfolio_open_func ( $atts ) {
   return '<div id="bd-portfolio-overlay">
   <div class="bd-portfolio-container">';
   }
-add_shortcode( 'bd-portfolio-open', 'bd_portfolio_open_func' );
+add_shortcode( 'bdp-open', 'bd_portfolio_open_func' );
 
 function bd_portfolio_content_func ( $atts ) {
   return '<div id="slider">
@@ -18,26 +18,26 @@ function bd_portfolio_content_func ( $atts ) {
   <a href="#" class="control_prev"></a>
   <ul>';
   }
-add_shortcode( 'bd-portfolio-content', 'bd_portfolio_content_func' );
+add_shortcode( 'bdp-content', 'bd_portfolio_content_func' );
 
 function bd_portfolio_content_end_func ( $atts ) {
   return '</ul>';
   }
-add_shortcode( 'bd-portfolio-content-end', 'bd_portfolio_content_end_func' );
+add_shortcode( 'bdp-content-end', 'bd_portfolio_content_end_func' );
 
 function bd_portfolio_close_func ( $atts ) {
   return '</div>
   </div>
   </div>';
   }
-add_shortcode( 'bd-portfolio-close', 'bd_portfolio_close_func' );
+add_shortcode( 'bdp-close', 'bd_portfolio_close_func' );
 
 // Overlay info
 function bd_portfolio_overlay_info_func ( $atts, $content = null  ) {
 return '<div id="overlay-info" class="overlay-info-hidden">
 <div class="overlay-info-content">' . $content . '</div></div>';
 }
-add_shortcode( 'bd-portfolio-more-info', 'bd_portfolio_overlay_info_func' );
+add_shortcode( 'bdp-more-info', 'bd_portfolio_overlay_info_func' );
 // Output portfolio image
 function bd_portfolio_image( $atts, $content = null ) {
   $xpath = new DOMXPath(@DOMDocument::loadHTML($content));
@@ -52,7 +52,7 @@ function bd_portfolio_image( $atts, $content = null ) {
   }
   return '<li class="bdp-non-text-slide"><img src="'. $strippedsrc . '" class="bd-portfolio-image '.$image_class.'" alt=""/></li>';
 }
-add_shortcode( 'bd-portfolio-image', 'bd_portfolio_image' );
+add_shortcode( 'bdp-image', 'bd_portfolio_image' );
 
 // Output portfolio text
 function bd_portfolio_text( $atts, $content = null ) {
@@ -60,21 +60,21 @@ function bd_portfolio_text( $atts, $content = null ) {
   return '<li class="bdp-text-slide">
   <div class="bdp-text">' . $content . '</div></li>';
 }
-add_shortcode( 'bd-portfolio-text', 'bd_portfolio_text' );
+add_shortcode( 'bdp-text', 'bd_portfolio_text' );
 
 // Output Youtube video
 function bd_portfolio_youtube( $atts, $content = null ) {
   $src = preg_replace('#youtube.com/embed/(.*)\?feature=oembed#','youtube.com/embed/$1?feature=oembed&version=3&enablejsapi=1',$content);
   return '<li class="bdp-non-text-slide">'.$src.'</li>';
 }
-add_shortcode( 'bd-portfolio-youtube', 'bd_portfolio_youtube' );
+add_shortcode( 'bdp-youtube', 'bd_portfolio_youtube' );
 
 // Output Vimeo video
 function bd_portfolio_vimeo( $atts, $content = null ) {
 $src = preg_replace('#<iframe src="https://player.vimeo.com/video/([0-9]{8,12})#','<iframe class="bdp-vimeo" src="https:// player.vimeo.com/video/$1?api=1&player_id=' . $content_id . '',$content);
 return '<li class="bdp-non-text-slide">'.$content.'</li>';
 }
-add_shortcode( 'bd-portfolio-vimeo', 'bd_portfolio_vimeo' );
+add_shortcode( 'bdp-vimeo', 'bd_portfolio_vimeo' );
 
 // Output the titles of portfolios who are in the same category
 function bd_portfolio_samecat( $atts ) {
@@ -102,4 +102,4 @@ function bd_portfolio_samecat( $atts ) {
  $links = implode(' ', $links_body);
  return $init.$links.$exit;
 }
-add_shortcode( 'bd-portfolio-samecategory', 'bd_portfolio_samecat' );
+add_shortcode( 'bdp-samecategory', 'bd_portfolio_samecat' );
